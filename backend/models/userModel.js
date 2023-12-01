dc58a2503dd1e1dded0aab7a433386e320ba759a
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-12-01 23:20:56 +1100

    admin users edit issue was fixed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     resetToken: { type: String },
-    isAdmin: { type: String, required: true, default: false },
+    isAdmin: { type: Boolean, required: true, default: false },
   },
   {
     timestamps: true,
